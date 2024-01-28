c9759fe12e33f4a5f9ac9e86db95bd97e85bdecf
Chrythm <68961747+Chrythm@users.noreply.github.com>
2024-01-27 22:26:53 -0600

    Add new songs to music list

--- src/components/music/music.jsx
+++ src/components/music/music.jsx
@@ -9,20 +9,29 @@ let Songs = {
             { Name: "The Mountains", Album: "Plush Wars 2", "URI": "songs/games/example_gamesoundtrack3.mp3" },
             { Name: "Castle", Album: "Plush Wars 2", "URI": "songs/games/example_gamesoundtrack4.mp3" },
             { Name: "Candy Desert", Album: "Plush Wars 2", "URI": "songs/games/example_gamesoundtrack5.mp3" },
-            { Name: "Oak's Desert", Album: "Oaklands", "URI": "songs/games/example_gamesoundtrack6.mp3" }
+            { Name: "Oak's Desert", Album: "Oaklands", "URI": "songs/games/example_gamesoundtrack6.mp3" },
+            { Name: "Funky Forest", Album: "Plush Wars 2", "URI": "songs/games/example_gamesoundtrack7.mp3" }
     ],
     Piano: [{ Name: "Coral Reef", Album: "stupid adventure", "URI": "songs/piano/example_pianosolo.mp3" },
-            { Name: "The Mall", Album: "stupid adventure", "URI": "songs/piano/example_pianosong.mp3" }
+            { Name: "The Mall", Album: "stupid adventure", "URI": "songs/piano/example_pianosong.mp3" },
+            { Name: "Rising Saplings", Album: "Oaklands", "URI": "songs/piano/example_piano2.mp3" }
     ],
     EDM: [{ Name: "Winds of Fjords (Cover)", Album: "Minomus", "URI": "songs/edm/example_edmcover.mp3" },
           { Name: "the_floaters.mp3", Album: "Single", "URI": "songs/edm/example_edmoriginal.mp3" },
           { Name: "Deadly Deep", Album: "stupid adventure", "URI": "songs/edm/example_edmoriginalgrungy.mp3" },
-          { Name: "Road to Kalimba", Album: "Doby's Clay World", "URI": "songs/edm/example_edmoriginalgrungy2.mp3" }
+          { Name: "Road to Kalimba", Album: "Doby's Clay World", "URI": "songs/edm/example_edmoriginalgrungy2.mp3" },
+          { Name: "Alan's Autologistics", Album: "Oaklands", "URI": "songs/edm/example_edm2.mp3" },
+          { Name: "Pianoman Watches", Album: "Hexagon Smash", "URI": "songs/edm/example_edm3.mp3" },
+          { Name: "Serious Business", Album: "Hexagon Smash", "URI": "songs/edm/example_edm4.mp3" },
+          { Name: "Toy Factory", Album: "Plush Wars 2", "URI": "songs/edm/example_edm5.mp3" }
     ],
-    Surreal: [{ Name: "Rainy Swamp", Album: "Doby's Clay World", "URI": "songs/surreal/example_surrealgrungy.mp3" }],
+    Surreal: [{ Name: "Rainy Swamp", Album: "Doby's Clay World", "URI": "songs/surreal/example_surrealgrungy.mp3" },
+              { Name: "Tattered Desert", Album: "The Legend of The Bone Sword", "URI": "songs/surreal/example_surreal2.mp3" }
+    ],
     Cinematic: [{ Name: "Dangus Desert", Album: "stupid adventure", "URI": "songs/cinematic/example_cinematicdesert.mp3" },
                 { Name: "Battle Beach", Album: "Plush Wars 2", "URI": "songs/cinematic/example_cinematic2.mp3" },
-                { Name: "Introduction", Album: "The Lighthouse", "URI": "songs/cinematic/example_cinematic3.mp3" }
+                { Name: "Introduction", Album: "The Lighthouse", "URI": "songs/cinematic/example_cinematic3.mp3" },
+                { Name: "Lay of the Land", Album: "Oaklands", "URI": "songs/cinematic/example_cinematic4.mp3" }
     ]
 }
 
